ef8405bcab94a1442a5ee27399eb38f8083fd34d
Fabio Rocha <frocha@quansight.com>
2022-08-17 10:45:15 -0500

    [primTorch] refs for pdist and triu related ops
    ghstack-source-id: 59b059f76f09099203dffd1b2daf91aa00eac596
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82819

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -6697,13 +6697,47 @@ def sample_inputs_tril_triu(op_info, device, dtype, requires_grad, **kwargs):
     make_arg = partial(make_tensor, dtype=dtype, device=device, requires_grad=requires_grad)
     cases = (((M, M), ()),
              ((M, M), (2,),),
-             ((S, M, M), ()),
-             ((S, M, M), (2,)),
+             ((M, S), ()),
+             ((M, S), (-1,)),
+             ((M, M), (2,),),
+             ((S, M, S), ()),
+             ((S, M, S), (2,)),
              ((3, 3, S, S), ()),)
 
     for shape, args in cases:
         yield SampleInput(make_arg(shape), args=args)
 
+def sample_inputs_trilu_indices(op_info, device, dtype, requires_grad, **kwargs):
+    # (row, col, offset)
+    args_list = ((1, 1),
+                 (3, 3, 1),
+                 (3, 3, 2),
+                 (3, 3, 200),
+                 (3, 3, -1),
+                 (3, 3, -2),
+                 (3, 3, -200),
+                 (0, 3, 0),
+                 (3, 0, 0),
+                 (0, 0, 0),
+                 (20, 21, 0),
+                 (20, 21, 10),
+                 (20, 21, -10),
+                 (60, 3),
+                 (60, 3, 37),
+                 (60, 3, -37),
+                 (3, 60, 37),
+                 (3, 60, -37),
+                 # Large test cases below are deliberately commented out to speed up CI
+                 # tests and to avoid OOM error. When modifying implementations of
+                 # tril_indices and triu_indices, please enable these tests and make sure
+                 # they pass.
+                 # (2, 68435455, 3),
+                 # (5000, 5000),
+                 # (5000, 5000, 1234),
+                 # (5000, 5000, -1233),
+                 )
+    for args in args_list:
+        yield SampleInput(args[0], args=args[1:], kwargs={"dtype": dtype, "device": device})
 
 def sample_inputs_clone_contiguous(op_info, device, dtype, requires_grad, **kwargs):
     make_arg = partial(make_tensor, dtype=dtype, device=device, requires_grad=requires_grad)
@@ -16366,6 +16400,50 @@ op_db: List[OpInfo] = [
            supports_forward_ad=True,
            supports_fwgrad_bwgrad=True,
            sample_inputs_func=sample_inputs_tril_triu),
+    OpInfo('triu_indices',
+           dtypes=integral_types_and(),
+           sample_inputs_func=sample_inputs_trilu_indices,
+           ref=lambda h, w, ofs=0, dtype=torch.long, device='cpu' : np.array(np.triu_indices(h, ofs, w), dtype=dtype),
+           supports_out=False,
+           supports_autograd=False,
+           skips=(
+               # skip these tests since we have non tensor input
+               DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_noncontiguous_samples'),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_variant_consistency_eager'),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestJit', 'test_variant_consistency_jit'),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestMathBits', 'test_neg_view'),
+               # test_dtypes will find this works for float dtypes, but it doesn't really
+               # make sense to call this with floats
+               DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_dtypes'),
+               # for some small dtypes, the torch cpu implementation gives the wrong results
+               # when you run it for large input, so we skip those tests
+               DecorateInfo(unittest.skip('Skipped!'), 'TestDecomp', 'test_comprehensive',
+                            device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestDecomp', 'test_quick',
+                            device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+           )),
+    OpInfo('tril_indices',
+           dtypes=integral_types_and(),
+           sample_inputs_func=sample_inputs_trilu_indices,
+           ref=lambda h, w, ofs=0, dtype=torch.long, device='cpu' : np.array(np.tril_indices(h, ofs, w), dtype=dtype),
+           supports_out=False,
+           supports_autograd=False,
+           skips=(
+               # skip these tests since we have non tensor input
+               DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_noncontiguous_samples'),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_variant_consistency_eager'),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestJit', 'test_variant_consistency_jit'),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestMathBits', 'test_neg_view'),
+               # test_dtypes will find this works for float dtypes, but it doesn't really
+               # make sense to use this with floats
+               DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_dtypes'),
+               # for some small dtypes, the torch cpu implementation gives the wrong results
+               # when you run it for large input, so we skip those tests
+               DecorateInfo(unittest.skip('Skipped!'), 'TestDecomp', 'test_comprehensive',
+                            device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+               DecorateInfo(unittest.skip('Skipped!'), 'TestDecomp', 'test_quick',
+                            device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+           )),
     OpInfo('kron',
            dtypes=all_types_and_complex_and(torch.bool, torch.half, torch.bfloat16),
            dtypesIfCUDA=all_types_and_complex_and(torch.bool, torch.half, torch.bfloat16),
@@ -18846,6 +18924,54 @@ python_ref_db = [
         supports_nvfuser=False,
     ),
     PythonRefInfo(
+        "_refs.triu",
+        torch_opinfo_name="triu",
+        supports_nvfuser=False,
+    ),
+    PythonRefInfo(
+        "_refs.tril",
+        torch_opinfo_name="tril",
+        supports_nvfuser=False,
+    ),
+    PythonRefInfo(
+        "_refs.triu_indices",
+        torch_opinfo_name="triu_indices",
+        supports_nvfuser=False,
+        # the implementation uses torch.stack that violates view consistency
+        validate_view_consistency=False,
+        skips=(
+            # skip these tests since we have non tensor input
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_noncontiguous_samples'),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_variant_consistency_eager'),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestJit', 'test_variant_consistency_jit'),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestMathBits', 'test_neg_view'),
+            # for some small dtypes, the torch cpu implementation gives the wrong results
+            # when you run it for large input, so we skip those tests
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_python_ref',
+                         device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_python_ref_torch_fallback',
+                         device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+        )),
+    PythonRefInfo(
+        "_refs.tril_indices",
+        torch_opinfo_name="tril_indices",
+        supports_nvfuser=False,
+        # the implementation uses torch.stack that violates view consistency
+        validate_view_consistency=False,
+        skips=(
+            # skip these tests since we have non tensor input
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_noncontiguous_samples'),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_variant_consistency_eager'),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestJit', 'test_variant_consistency_jit'),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestMathBits', 'test_neg_view'),
+            # for some small dtypes, the torch cpu implementation gives the wrong results
+            # when you run it for large input, so we skip those tests
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_python_ref',
+                         device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+            DecorateInfo(unittest.skip('Skipped!'), 'TestCommon', 'test_python_ref_torch_fallback',
+                         device_type="cpu", dtypes=(torch.uint8, torch.int8, torch.int16)),
+        )),
+    PythonRefInfo(
         "_refs.meshgrid",
         torch_opinfo_name="meshgrid",
         torch_opinfo_variant_name="list_of_tensors",
@@ -19178,6 +19304,20 @@ python_ref_db = [
         supports_nvfuser=False,
         supports_out=True,
     ),
+    PythonRefInfo(
+        "_refs.nn.functional.pairwise_distance",
+        torch_opinfo_name="nn.functional.pairwise_distance",
+        supports_out=True,
+    ),
+    PythonRefInfo(
+        "_refs.nn.functional.pdist",
+        torch_opinfo_name="nn.functional.pdist",
+        supports_out=True,
+        supports_nvfuser=False,
+        skips=(
+            # RunTimeError: no _refs support for torch.Tensor.index_select
+            DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_python_ref'),
+        )),
     PythonRefInfo(
         "_refs.nn.functional.leaky_relu",
         torch_opinfo_name="nn.functional.leaky_relu",
